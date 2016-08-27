275bd6bb338741a77de38f4b0346c8331292f563
Anuj <aab688@nyu.edu>
2016-08-27 13:51:10 +0530

    Added Search in Rotated Sorted Array II

---
+++ Search in Rotated Sorted Array II/README.md
@@ -0,0 +1,9 @@
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return true, otherwise return false.
+
+Duplicate may exist in the array.
+
+LeetCode : https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
---
+++ Search in Rotated Sorted Array II/Search.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+/**
+ *  Time Complexity = O(log n)
+ */
+
+bool search(vector<int>& nums, int target) 
+{
+    int low = 0;
+    int high = nums.size() - 1;
+        
+    while (low <= high)
+    {
+        while (low < high && nums[low] == nums[low + 1])
+        {
+            low++;
+        }
+        while (high > low && nums[high] == nums[high - 1])
+        {
+            high--;
+        }
+
+        int mid = low + (high-low) / 2;
+        
+        if (nums[mid] == target)
+        {
+            return true;
+        }
+        else if (nums[mid] > target)
+        {
+            if(nums[low] <=target || nums[mid] < nums[low])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        else
+        {
+            if (nums[low] > target || nums[mid] >= nums[low])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+    
+    return false;  
+}
